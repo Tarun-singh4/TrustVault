58b6df448f063ad53ca05a81b940d9d2ddbab606
Tarun <tarunsingh95409@gmail.con>
2026-04-05 12:46:08 +0530

    calling the database into server

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,10 @@
 const app=require("./src/app.js");
+const dotenv=require("dotenv");
+const connectDB=require("./src/db/db.js");
+dotenv.config();
 
+connectDB();
 
-
-app.listen(3000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("server is running on port 3000");
 })
